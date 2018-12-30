e31f99a1674849198086b193643c094be246a9ae
Bence Rigler <rigbence@gmail.com>
2018-12-30 22:30:28 +0100

    Search result pagination
    Implementing Search result pagination, previous and next buttons

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,12 +2,13 @@ export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
     searchResList: document.querySelector('.results__list'),
-    searchRes: document.querySelector('.results')
+    searchRes: document.querySelector('.results'),
+    searchResPages: document.querySelector('.results__pages')
 };
+//Rendering Loading Spinner
 export const elementStrings = {
     loader: 'loader'
 };
-
 export const renderLoader = parent => {
     const loader = `
         <div class="${elementStrings.loader}">
@@ -18,7 +19,7 @@ export const renderLoader = parent => {
     `;
     parent.insertAdjacentHTML('afterbegin', loader);
 };
-
+// removing the loading spinner
 export const clearLoader = () => {
     const loader = document.querySelector(`.${elementStrings.loader}`);
     if (loader)  loader.parentElement.removeChild(loader);
